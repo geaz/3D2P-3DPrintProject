343add65d100d2a8d2e6ed9d65352b2bf5139280
Gerrit 'Geaz' Gazic <geaz@users.noreply.github.com>
2020-05-05 20:25:18 +0200

    Add WIX

---
+++ src/desktop/wix.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Build app.js
+call npm --prefix ..\app run build:desktop
+
+echo Build Project
+call dotnet publish --runtime win-x64 --configuration Release
+
+echo Harvest ApplicationFiles
+call heat dir bin/Release/netcoreapp3.1/win-x64/publish -o Files.wxs -dr INSTALLDIR -cg ApplicationFiles -gg -sreg -srd -scom
+
+echo Executing Candle
+call candle PrintProjects.App.wxs Files.wxs -ext WixUIExtension -ext WixUtilExtension
+
+echo Executing Light 
+call light PrintProjects.App.wixobj Files.wixobj -ext WixUIExtension -ext WixUtilExtension -b bin/Release/netcoreapp3.1/win-x64/publish -o 3D2P
