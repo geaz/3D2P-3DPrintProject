52a0e9d2cd3889d26a4c2bea2b63f62f6085f31a
Gerrit Gazic <geaz@users.noreply.github.com>
2020-05-13 08:43:03 +0200

    Add Github Workflows
    Workflows for Visual Studio Code Extension and CLI Build

--- src/desktop/wix.bat
+++ src/desktop/wix.bat
@@ -6,11 +6,14 @@ call npm --prefix ..\app run build:desktop
 echo Build Project
 call dotnet publish PrintProject.App/PrintProject.App.csproj --runtime win-x64 --configuration Release
 
+call mkdir wixBuild
+call cd wixBuild
+
 echo Harvest ApplicationFiles
-call heat dir PrintProject.App/bin/Release/netcoreapp3.1/win-x64/publish -o Files.wxs -dr INSTALLDIR -cg ApplicationFiles -gg -sreg -srd -scom
+call heat dir ../PrintProject.App/bin/Release/netcoreapp3.1/win-x64/publish -o Files.wxs -dr INSTALLDIR -cg ApplicationFiles -gg -sreg -srd -scom
 
 echo Executing Candle
-call candle PrintProject.App/PrintProject.App.wxs Files.wxs -ext WixUIExtension -ext WixUtilExtension
+call candle ../PrintProject.App/PrintProject.App.wxs Files.wxs -ext WixUIExtension -ext WixUtilExtension
 
 echo Executing Light 
-call light PrintProject.App.wixobj Files.wixobj -ext WixUIExtension -ext WixUtilExtension -b PrintProject.App/bin/Release/netcoreapp3.1/win-x64/publish -o 3D2P
+call light PrintProject.App.wixobj Files.wixobj -ext WixUIExtension -ext WixUtilExtension -b ../PrintProject.App/bin/Release/netcoreapp3.1/win-x64/publish -o 3D2P
---
+++ src/desktop/wix.ps1
@@ -0,0 +1,20 @@
+echo "Setting WIX Path"
+$env:Path+=";$env:WIX\bin"
+
+echo "Build app.js"
+npm --prefix ..\app run build:desktop
+
+echo "Build Project"
+dotnet publish PrintProject.App/PrintProject.App.csproj --runtime win-x64 --configuration Release
+
+mkdir wixBuild
+cd wixBuild
+
+echo "Harvest ApplicationFiles"
+heat dir ../PrintProject.App/bin/Release/netcoreapp3.1/win-x64/publish -o Files.wxs -dr INSTALLDIR -cg ApplicationFiles -gg -sreg -srd -scom
+
+echo "Executing Candle"
+candle ../PrintProject.App/PrintProject.App.wxs Files.wxs -ext WixUIExtension -ext WixUtilExtension
+
+echo "Executing Light"
+light PrintProject.App.wixobj Files.wixobj -ext WixUIExtension -ext WixUtilExtension -b ../PrintProject.App/bin/Release/netcoreapp3.1/win-x64/publish -o 3D2P
